47f0a8d5bd98bb1d75128891f7872ee1e56274d0
temarych <teemest06@icloud.com>
2024-11-26 23:10:25 +0200

    feat(check): update selection logic

--- src/modules/check/helpers/getTextPositions.ts
+++ src/modules/check/helpers/getTextPositions.ts
@@ -1,11 +1,11 @@
 export const getTextPositions = (sourceText: string, searchText: string) => {
   if (!sourceText || !searchText) return null;
 
-  const startIndex = sourceText.indexOf(searchText);
+  const startIndex = sourceText.indexOf(searchText) + 1;
 
-  if (startIndex === -1) return null;
+  if (startIndex === 0) return null;
 
   const endIndex = startIndex + searchText.length;
 
-  return { start: startIndex + 1, end: endIndex };
+  return { start: startIndex, end: endIndex };
 };
